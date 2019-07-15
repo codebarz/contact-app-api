ed827d19e1277e7f8c69ef2aaa2bfa754e6642f8
Tega Oke <oketegah@gmail.com>
2019-07-15 17:19:00 +0100

    Tested function to get contacts

--- __tests__/routes.ts
+++ __tests__/routes.ts
@@ -67,3 +67,17 @@ describe('Getting Specific data', () => {
       });
   });
 });
+
+describe('Controllers', () => {
+  test('Should check if contacts can be gotten', async () => {
+    const data = await getContacts();
+    expect(data).toContainEqual({
+      id: expect.any(String),
+      fullName: expect.any(String),
+      phone: expect.any(String),
+      email: expect.any(String),
+      company: expect.any(String),
+      isBlocked: expect.any(Boolean)
+    });
+  });
+});
