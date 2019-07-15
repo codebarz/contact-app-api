c618174c87a35e2c6f2aa4f6840f12b3bba86af5
Tega Oke <oketegah@gmail.com>
2019-07-15 17:21:01 +0100

    Moved controllers test to controllers test file

---
+++ __tests__/controllers.ts
@@ -0,0 +1,15 @@
+import { getContacts } from '../src/controllers';
+
+describe('Controllers', () => {
+  test('Should check if contacts can be gotten', async () => {
+    const data = await getContacts();
+    expect(data).toContainEqual({
+      id: expect.any(String),
+      fullName: expect.any(String),
+      phone: expect.any(String),
+      email: expect.any(String),
+      company: expect.any(String),
+      isBlocked: expect.any(Boolean)
+    });
+  });
+});
