42f4378d4ddc4f85f99322c9ab80806ddb29289a
Tega Oke <oketegah@gmail.com>
2019-07-16 05:01:45 +0100

    Fixed array bug in addContact controller (used spread synthax)

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -33,7 +33,7 @@ export function addContact(details: IDetails) {
   let addedToExisting = false;
   contacts.forEach((contact: any) => {
     if (contact.fullName === details.fullName) {
-      contact.phone = [contact.phone];
+      contact.phone = [...contact.phone];
       contact.phone.push(details.phone);
       addedToExisting = true;
       return;
